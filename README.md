85d89404faf565995c63d6d71ae6e05a781b5bf4
sypai <7suyashbajpai@gmail.com>
2019-01-16 18:18:56 +0530

    Portfolio Ready!

---
+++ README.md
@@ -0,0 +1,2 @@
+# sillycoder713.github.io
+A modern portfolio website, which is responsive and looks elegant!
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,36 @@
-// Select DOM items
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
+//Select DOM items
 
-// Set Initial state of menu
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+
+//Set Initial state of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
     if(!showMenu){
-        menuBtn.classList.add("close");
-        menu.classList.add("show");
-        menuNav.classList.add("show");
-        menuBranding.classList.add("show");
+      menuBtn.classList.add('close');
+      menu.classList.add('show');
+      menuNav.classList.add('show');
+      menuBranding.classList.add('show');
+      navItems.forEach(item => item.classList.add('show'));
 
-        navItems.forEach(item => item.classList.add('show'));
-
-        //Set Menu state
-        showMenu = true;
+      //Set Menu state
+      showMenu = true ;
     }
 
     else{
-        menuBtn.classList.remove("close");
-        menu.classList.remove("show");
-        menuNav.classList.remove("show");
-        menuBranding.classList.remove("show");
+     menuBtn.classList.remove('close');
+     menu.classList.remove('show');
+     menuNav.classList.remove('show');
+     menuBranding.classList.remove('show');
+     navItems.forEach(item => item.classList.remove('show'))
 
-        navItems.forEach(item => item.classList.remove('show'));
-
-        //Set Menu state
-        showMenu = false;
+     showMenu = false ;
     }
 }
